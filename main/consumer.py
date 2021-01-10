352cfd4d636ad25356a4da1d25ed9ece4c9b65ca
Gordon <kongcc95@gmail.com>
2021-01-10 22:30:40 +0800

    django and flask microservices

---
+++ main/consumer.py
@@ -0,0 +1,39 @@
+import pika, json
+
+from main import Product, db
+
+params = pika.URLParameters('amqps://xdmidven:S42rEdFyXISmFTRFsztqpWwyQMWdiX0e@owl.rmq.cloudamqp.com/xdmidven')
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+channel.queue_declare(queue='main')
+
+
+def callback(ch, method, properties, body):
+    print('Received in main')
+    data = json.loads(body)
+    print(data)
+
+    if properties.content_type == 'product_created':
+        product = Product(id=data['id'], title=data['title'], image=data['image'])
+        db.session.add(product)
+        db.session.commit()
+        print('Product created in main')
+
+    elif properties.content_type == 'product_updated':
+        product = Product.query.get(data['id'])
+        product.title = data['title']
+        product.image = data['image']
+        db.session.commit()
+        print('Product updated in main')
+
+    elif properties.content_type == 'product_deleted':
+        product = Product.query.get(data)
+        db.session.delete(product)
+        db.session.commit()
+        print('Product deleted in main')
+
+
+channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
+print('Started Consuming...')
+channel.start_consuming()
+channel.close()
